12b74be15d7c1bbf3f092879e3ecf5c65e439d15
felipeabreu-dev <abreufelipe12@hotmail.com>
2026-02-04 08:48:33 -0300

    Criação do Projeto

---
+++ src/test/java/dev/felipeabreu/agregador_de_investimentos/AgregadorDeInvestimentosApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.felipeabreu.agregador_de_investimentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgregadorDeInvestimentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
